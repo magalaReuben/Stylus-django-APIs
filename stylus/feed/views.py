659308be7996712d6ba481e25f3ebed9771a0587
magalaReuben <magalareuben60@gmail.com>
2023-02-18 12:55:42 +0300

    Added restframework api

---
+++ stylus/feed/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+# Create your views here.
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        {
+            'Endpoint': '/signin',
+            'Method': 'GET',
+            'body': None,
+            'description': 'signin endpoint'
+        }
+    ]
+    return Response(routes)
